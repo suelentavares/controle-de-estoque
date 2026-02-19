d140451bf7f90bf3be08c3ad768aa0048f8aac80
suelentavares <94622266+suelentavares@users.noreply.github.com>
2026-02-19 15:26:31 -0300

    Update editar.php
    Como posso entender

--- estoque/editar.php
+++ estoque/editar.php
@@ -8,7 +8,7 @@ require_once BASE_PATH . '/includes/cabecalho.php';
 <section class="mb-4 border rounded-3 p-4 border-primary-subtle">
     <h3 class="text-center"><i class="bi bi-pencil-fill"></i> Editar Estoque</h3>
 
-
+    
     <form method="post" class="w-75 mx-auto">
         <div class="form-group">
             <label for="estoque" class="form-label">Quantidade em Estoque:</label>
